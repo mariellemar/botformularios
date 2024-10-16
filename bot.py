c188e22e494f3743bbca295654624b42e2b43812
mariellemar <mariellemarsantos@gmail.com>
2024-10-16 19:39:14 -0400

    Defininfo funções preencher login e contato

--- bot.py
+++ bot.py
@@ -1,78 +1,64 @@
-"""
-WARNING:
-
-Please make sure you install the bot dependencies with `pip install --upgrade -r requirements.txt`
-in order to get all the dependencies on your Python environment.
-
-Also, if you are using PyCharm or another IDE, make sure that you use the SAME Python interpreter
-as your IDE.
-
-If you get an error like:
-```
-ModuleNotFoundError: No module named 'botcity'
-```
-
-This means that you are likely using a different Python interpreter than the one used to install the dependencies.
-To fix this, you can either:
-- Use the same interpreter as your IDE and install your bot with `pip install --upgrade -r requirements.txt`
-- Use the same interpreter as the one used to install the bot (`pip install --upgrade -r requirements.txt`)
-
-Please refer to the documentation for more information at
-https://documentation.botcity.dev/tutorials/python-automations/web/
-"""
-
-
-# Import for the Web Bot
-from botcity.web import WebBot, Browser, By
-
-# Import for integration with BotCity Maestro SDK
 from botcity.maestro import *
+from faker import Faker
 
-# Disable errors if we are not connected to Maestro
+from FormContato import FormContato
+from FormLogin import FormLogin
+
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
 
+def autenticar(form_login: FormLogin, qtd: range):
+    fake = Faker()
+    
+    for _ in qtd:
+        nome = fake.name()
+        senha = fake.password()
+        
+        form_login.formulario_login()
+        form_login.nome()
+        form_login.senha()
+        form_login.enviar()
+        
+
+def adicionar_contato(form_contato: FormContato, qtd: range):
+    fake = Faker()
+    
+    for _ in qtd:
+        nome = fake.name()
+        email = fake.email()
+        tel = fake.phone_number()
+        
+        form_contato.formularios_contato()
+        form_contato.nome()
+        form_contato.email()
+        form_contato.telefone()
+        form_contato.enviar()
+
+
+
+
+
+
 def main():
-    # Runner passes the server url, the id of the task being executed,
-    # the access token and the parameters that this task receives (when applicable).
     maestro = BotMaestroSDK.from_sys_args()
-    ## Fetch the BotExecution with details from the task, including parameters
     execution = maestro.get_execution()
 
     print(f"Task ID is: {execution.task_id}")
     print(f"Task Parameters are: {execution.parameters}")
-
-    bot = WebBot()
-
-    # Configure whether or not to run on headless mode
-    bot.headless = False
-
-    # Uncomment to change the default Browser to Firefox
-    # bot.browser = Browser.FIREFOX
-
-    # Uncomment to set the WebDriver path
-    # bot.driver_path = "<path to your WebDriver binary>"
-
-    # Opens the BotCity website.
-    bot.browse("https://www.botcity.dev")
-
-    # Implement here your logic...
-    ...
-
-    # Wait 3 seconds before closing
-    bot.wait(3000)
-
-    # Finish and clean up the Web Browser
-    # You MUST invoke the stop_browser to avoid
-    # leaving instances of the webdriver open
-    bot.stop_browser()
-
-    # Uncomment to mark this task as finished on BotMaestro
-    # maestro.finish_task(
-    #     task_id=execution.task_id,
-    #     status=AutomationTaskFinishStatus.SUCCESS,
-    #     message="Task Finished OK."
-    # )
+    
+    try:
+        contato = FormContato()
+        login = FormLogin()
+    
+        autenticar(login, range(10))
+        adicionar_contato(contato, range(10))
+        
+    except Exception as e:
+            print(f"Error: {e}")
+    
+    finally:
+        contato.bot.stop_browser()
+        login.bot.stop_browser()
 
 
 def not_found(label):
