cc1efc103541a52aca5db03445fbee6ca4a90dac
mariellemar <mariellemarsantos@gmail.com>
2024-10-16 19:09:02 -0400

    Adicionando formulario de contato

--- FormContato.py
+++ FormContato.py
@@ -9,6 +9,28 @@ class FormContato(FormBase):
     
     def formularios_contato(self):
         relativo = 'botformularios\templates\form_contato.html'
+        caminho = os.path.abspath(relativo)
+        self.bot.browse(f'file:///{caminho.replace("\\", "/")}')
+        self.bot.wait(500)
         
-
+        
+    def nome(self, nome):
+        self.bot.find_element('//*[@id="nome"]', By.XPATH).send_keys(nome)
+        self.bot.wait(1000)
+        
+    
+    def email(self, e_mail):
+        self.bot.find_element('//*[@id="email"]', By.XPATH).send_keys(e_mail)
+        self.bot.wait(1000)
+        
+    
+    def telefone(self, telefone):
+        self.bot.find_element('//*[@id="telefone"]', By.XPATH).send_keys(telefone)
+        self.bot.wait(1000)
+        
+        
+    def enviar(self):
+        btn = self.bot.find_element('/html/body/div/form/input[4]', By.XPATH)
+        btn.click()
+        self.bot.wait(1000)
 
